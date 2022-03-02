2ca4222aa32c3a6bc9da04247f3fe2b2a3c53c91
AppleBoy <ikjy05@gmail.com>
2022-03-02 17:26:27 +0900

    아이폰 가로

--- TennisCounter/ContentView.swift
+++ TennisCounter/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var our = Team()
+    @State var your = Team(isYou: true)
+//    @State private var orientation = UIDeviceOrientation.unknown
+//    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            AdaptiveView {
+                TeamView(team: $our, your: $your)
+                    .background(Color(.black))
+                    .foregroundColor(.white)
+                
+                TeamView(team: $your, your: $our)
+                    .background(Color(.white))
+                    .foregroundColor(.black)
+            }
+            .onLongPressGesture {
+                our = Team()
+                your = Team(isYou: true)
+            }
+            .preferredColorScheme(.dark)
+            
+            
+        }
     }
 }
+
+
+struct AdaptiveView<Content: View>: View {
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+//    @Environment(\.sizeCategory) var sizeCategory: ContentSizeCategory
+    var content: Content
+    
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    
+    var body: some View {
+        if verticalSizeClass == .compact {
+            HStack {
+                content
+            }
+        } else {
+            VStack {
+                content
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
     }
 }
